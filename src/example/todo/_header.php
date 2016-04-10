01824e7b3da4db8ad1002ac5bc7f6489fc7edcab
Nick Cox <nickcox1008@gmail.com>
2016-04-10 17:53:20 -0600

    Begin a TODO example

--- src/example/todo/_header.php
+++ src/example/todo/_header.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Clear PHP</title>
+		<title>TODO app example - Clear PHP</title>
 	</head>
 	<body>
 		<pre>URL: <?= BASEURL ?> - Base: <?= BASEDIR ?> - Current: <?= CURDIR ?> - File: <?= CURFILE ?></pre>
---
+++ src/example/todo/index.php
@@ -0,0 +1,6 @@
+<?php require_once '../../clear.php' ?>
+<?php Clear::template('example/todo/_header') ?>
+
+<h1>TODO Items</h1>
+
+<?php Clear::template('example/todo/_footer') ?>
