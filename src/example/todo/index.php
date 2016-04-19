d571ebbc3fc80b0ce2d58ef2428dd7d7a8c0a80f
Nick Cox <nickcox1008@gmail.com>
2016-04-18 23:00:13 -0600

    Add bootstrap css to the todo example

--- src/example/todo/index.php
+++ src/example/todo/index.php
@@ -3,4 +3,35 @@
 
 <h1>TODO Items</h1>
 
+<form action="<?= RELURL ?>/create.php" method="POST" class="row">
+	<div class="col-sm-10">
+		<input type="text" name="name" class="form-control"/>
+	</div>
+	<div class="col-sm-2">
+		<button type="submit" class="btn btn-block btn-default">Add</button>
+	</div>
+</form>
+
+<pre><?= var_export($todos) ?></pre>
+<table class="table">
+	<thead>
+		<tr>
+			<th>Due</th>
+			<th>Name</th>
+			<th>Notes</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($todos as $i => $todo): ?>
+			<tr>
+				<td><?= $todo->due ?></td>
+				<td><?= $todo->name ?></td>
+				<td><?= $todo->notes ?></td>
+				<td><a href="<?= RELURL ?>/done.php?id=<?= $todo->id ?>" title="Mark as Complete">✓</a></td>
+			</tr>
+		<?php endforeach ?>
+	</tbody>
+</table>
+
 <?php Clear::template('example/todo/_footer') ?>
