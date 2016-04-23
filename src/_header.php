5a4280bb148667c2c8d1cfe0d7ac779e12a2d888
Nick Cox <nickcox1008@gmail.com>
2016-04-22 22:12:46 -0600

    Add jQuery

--- src/_header.php
+++ src/_header.php
@@ -2,6 +2,7 @@
 	<head>
 		<title>Clear PHP</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
 	</head>
 	<body>
 		<div class="container">
