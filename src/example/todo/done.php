16b1390b7b694f8d4a95447440a17a44a6b96b64
Nick Cox <nickcox1008@gmail.com>
2016-04-22 22:15:22 -0600

    Set todo done date

--- src/example/todo/done.php
+++ src/example/todo/done.php
@@ -5,7 +5,10 @@ Clear::model('example/todo/todo');
 $model = new TodoModel();
 
 if (isset($_GET['id'])) {
-	$model->set(array('done' => 1), $_GET['id']);
+	$model->set(array(
+		'done' => 1,
+		'date = CURRENT_TIMESTAMP',
+	), $_GET['id']);
 }
 
 Clear::redirect('/example/todo/');
