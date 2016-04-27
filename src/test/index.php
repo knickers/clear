a3a5f8a450f411d71f10c26940a37f626be2d6ce
Nick Cox <nickcox1008@gmail.com>
2016-04-26 18:39:41 -0600

    Cleaner form HTML

--- src/test/index.php
+++ src/test/index.php
@@ -12,12 +12,17 @@
 <h2>MySQL Debuging</h2>
 
 <form action="" method="POST">
-	<button <?= $debug ? '' : 'type="submit"' ?> name="debug" value="on" class="<?= $debug ? 'active' : '' ?> btn btn-default">
-		Enable
-	</button>
-	<button <?= $debug ? 'type="submit"' : '' ?> name="debug" value="off" class="<?= $debug ? '' : 'active' ?> btn btn-default">
-		Disable
-	</button>
+	<?php if ($debug): ?>
+		<div class="active btn btn-default">Enable</div>
+		<button type="submit" name="debug" value="off" class="btn btn-default">
+			Disable
+		</button>
+	<?php else: ?>
+		<button type="submit" name="debug" value="on" class="btn btn-default">
+			Enable
+		</button>
+		<div class="active btn btn-default">Disable</div>
+	<?php endif ?>
 </form>
 
 <?php Clear::template('_footer') ?>
