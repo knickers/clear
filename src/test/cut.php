e8d8edde99ba38bfa7110965ec097e0392858616
Nick Cox <nickcox1008@gmail.com>
2018-01-27 10:47:25 -0700

    Change cut test to use an html table template

--- src/test/cut.php
+++ src/test/cut.php
@@ -1,41 +1,39 @@
 <?php
 require_once '../clear.php';
 
-$echo = "'%s' - '%s' = '%s' : %s<br>";
+$echo = '<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>';
 $cut = '.php';
 $yes = 'index';
 
-echo 'rcut:<br>';
+echo '<style>th, td { padding: 5px 5px 3px; }</style><table border="1">';
+echo '<tr><th>Input</th><th>Cut</th><th>Result</th><th>Pass</th></tr>';
+echo '<tr><th colspan="4" style="text-align:left;">rcut</th></tr>';
 
-$rcut = [
-	'index', 'index.php',
-];
+$rcut = ['index', 'index.php'];
 foreach($rcut as $str) {
 	$got = rcut($str, $cut);
 	$ok = $got == $yes ? 'pass' : 'fail';
 	echo sprintf($echo, $str, $cut, $got, $ok);
 }
 
-echo '<hr>lcut:<br>';
+echo '<tr><th colspan="4" style="text-align:left;">lcut</th></tr>';
 
 $cut = 'home/';
-$lcut = [
-	'index', 'home/index',
-];
+$lcut = ['index', 'home/index'];
 foreach($lcut as $str) {
 	$got = lcut($str, $cut);
 	$ok = $got == $yes ? 'pass' : 'fail';
 	echo sprintf($echo, $str, $cut, $got, $ok);
 }
 
-echo '<hr>cut:<br>';
+echo '<tr><th colspan="4" style="text-align:left;">cut</th></tr>';
 
 $cut = 'hello';
-$lcut = [
-	'index', 'helloindex', 'indexhello', 'helloindexhello',
-];
+$lcut = ['index', 'helloindex', 'indexhello', 'helloindexhello'];
 foreach($lcut as $str) {
 	$got = cut($str, $cut);
 	$ok = $got == $yes ? 'pass' : 'fail';
 	echo sprintf($echo, $str, $cut, $got, $ok);
 }
+
+echo '</table>';
