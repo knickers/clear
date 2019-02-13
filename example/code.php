17763b62464550c061f9677e0d5ad3a774b046fd
Nick Cox <nickcox1008@gmail.com>
2019-02-13 16:21:31 -0700

    Code blocks

---
+++ example/code.php
@@ -0,0 +1,13 @@
+<?
+require 'header.pht';
+require 'nav.pht';
+?>
+
+<div class="container my-4">
+	<? $codeFilename = 'clear.php'; include 'code-block.pht' ?>
+</div>
+
+<?
+require 'footer.pht';
+require 'close.pht';
+?>
