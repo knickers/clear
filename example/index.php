0718085a499e910584002426893fab6927c74901
Nick Cox <nickcox1008@gmail.com>
2019-02-13 16:29:14 -0700

    A template for the hero image slider

--- example/index.php
+++ example/index.php
@@ -1,57 +1,10 @@
 <?
 require 'header.pht';
 require 'nav.pht';
-?>
 
-<div id="product-carousel"
-	class="carousel slide"
-	data-ride="carousel"
->
-	<div class="carousel-inner">
-		<div class="carousel-item active">
-			<img class="d-block w-100"
-				src="/img/wide/bavarian_alps_mountains_lake_berchtesgaden_germany.jpg"
-				alt="First slide"
-			>
-		</div>
-		<div class="carousel-item">
-			<img class="d-block w-100"
-				src="/img/wide/country_road_hill.jpg"
-				alt="First slide"
-			>
-		</div>
-		<div class="carousel-item">
-			<img class="d-block w-100"
-				src="/img/wide/most_beautiful_waterscapes.jpg"
-				alt="First slide"
-			>
-		</div>
-		<div class="carousel-item">
-			<img class="d-block w-100"
-				src="/img/wide/sun_shining_through_wilderness.jpg"
-				alt="First slide"
-			>
-		</div>
-	</div>
-	<a class="carousel-control-prev"
-		href="#product-carousel"
-		role="button"
-		data-slide="prev"
-	>
-		<span class="carousel-control-prev-icon" aria-hidden="true">
-		</span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="carousel-control-next"
-		href="#product-carousel"
-		role="button"
-		data-slide="next"
-	>
-		<span class="carousel-control-next-icon" aria-hidden="true">
-		</span>
-		<span class="sr-only">Next</span>
-	</a>
-</div>
+$imageDirectory = 'img/wide/';
+include 'hero-carousel.pht';
+?>
 
 <div class="container my-4">
 	<h1>Hello from index</h1>
