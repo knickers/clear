4c63353d7bf7ed0e613cf13990c5dd55869a0ab3
Nick Cox <nickcox1008@gmail.com>
2019-02-16 16:24:48 -0700

    Different code block

--- example/code.php
+++ example/code.php
@@ -1,10 +1,21 @@
 <?
+require_once 'clear.php';
 require 'header.pht';
 require 'nav.pht';
 ?>
 
 <div class="container my-4">
-	<? $codeFilename = 'clear.php'; include 'code-block.pht' ?>
+	<div class="row">
+		<div class="col-md-6">
+			<div>controller.php</div>
+			<? $codeFilename = 'contact.php'; require 'code-block.pht' ?>
+		</div>
+		<div class="col-md-6">
+			<div>controller.phc</div>
+			<? $codeFilename = 'contact.phc'; require 'code-block.pht' ?>
+		</div>
+	</div>
+	<?// $codeFilename = 'clear.php'; require 'code-block.pht' ?>
 </div>
 
 <?
